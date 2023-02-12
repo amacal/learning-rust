219aa926fbc74740edd16eeac8c4d1f141c03246
Adrian Macal <adrian.macal@vistaprint.com>
2023-02-12 11:13:21 +0000

    Async main.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
-use tokio::runtime::Runtime;
-use tokio::time::sleep;
-use tokio::time::timeout;
-use tokio::time::Duration;
+use tokio::time::{sleep, timeout, Duration};
 
-fn main() {
-    let runtime = Runtime::new().unwrap();
-
-    let task = runtime.spawn(async {
+#[tokio::main]
+async fn main() {
+    let task = tokio::spawn(async {
         println!("Hello from the executor.");
         sleep(Duration::from_secs(5)).await;
         "success"
     });
 
-    runtime.block_on(async {
-        match timeout(Duration::from_secs(10), task).await {
-            Ok(result) => println!("completed: {}", result.unwrap()),
-            Err(_) => println!("failed: timed out!"),
-        };
-    });
+    match timeout(Duration::from_secs(10), task).await {
+        Ok(result) => println!("completed: {}", result.unwrap()),
+        Err(_) => println!("failed: timed out!"),
+    };
 }
