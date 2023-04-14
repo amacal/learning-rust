8455d1ecd248729d55d6cd5daa631c582b4188cd
Adrian Macal <adrian.macal@vistaprint.com>
2023-04-14 11:28:44 +0200

    Initial check.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
-mod cli;
-mod databricks;
-mod environment;
-mod helpers;
+use std::error::Error;
 
-use std::process;
-use cli::CliError;
-use structopt::StructOpt;
-
-use self::cli::{handle_dbfs_get, handle_dbfs_list, Dbfs};
-
-fn exit_with_error(error: CliError) -> ! {
-    println!("{}", error);
-    process::exit(-1);
-}
+use hyper::client::HttpConnector;
+use hyper::{Body, Client, Request};
 
 #[tokio::main]
-async fn main() {
-    let result = match Dbfs::from_args() {
-        Dbfs::Get(args) => handle_dbfs_get(args).await,
-        Dbfs::List(args) => handle_dbfs_list(args).await,
-    };
+async fn main() -> Result<(), Box<dyn Error>> {
+    let request = Request::builder()
+        .method(hyper::http::Method::GET)
+        .uri("http://mirror.accum.se/mirror/wikimedia.org/dumps/enwiki/20230401/enwiki-20230401-abstract.xml.gz")
+        .body(Body::empty())?;
 
-    match result {
-        Err(error) => exit_with_error(error),
-        Ok(_) => return,
+    let http = HttpConnector::new();
+    let client = Client::builder().build::<_, Body>(http);
+
+    let mut response = client.request(request).await?;
+    println!("Status: {}", response.status());
+
+    while response.status().is_redirection() {
+        let location = response.headers().get(hyper::header::LOCATION).unwrap().to_str().unwrap();
+        println!("Following redirect to: {}", location);
+
+        let request = Request::builder()
+            .method(hyper::http::Method::GET)
+            .uri(location)
+            .body(Body::empty())?;
+
+        response = client.request(request).await?;
+        println!("Status: {}", response.status());
     }
+
+    let body = response.into_body();
+    println!("Body: {:?}", body);
+
+    let bytes = hyper::body::to_bytes(body).await?;
+    println!("Bytes: {:?}", bytes);
+
+    Ok(())
 }
