a756ac120e6a5bab5b5556603d74785ffd70f5ea
Adrian Macal <amacal@id5.io>
2025-06-08 20:34:54 +0000

    Add Into/From abstraction.

--- src/avl.rs
+++ src/avl.rs
@@ -100,38 +100,44 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy> Node<T, K, V> {
     }
 }
 
-pub struct AvlForest<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> {
+pub struct AvlForest<T: Copy, K: Copy + PartialOrd, V: Copy, N: Copy, A: NodeArena<N>> {
     arena: A,
     key: PhantomData<K>,
     value: PhantomData<V>,
     tree: PhantomData<T>,
+    into: PhantomData<N>,
 }
 
-impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> AvlForest<T, K, V, A> {
+impl<T: Copy, K: Copy + PartialOrd, V: Copy, N: Copy, A: NodeArena<N>> AvlForest<T, K, V, N, A> {
     pub fn new(arena: A) -> Self {
-        AvlForest { arena, key: PhantomData, value: PhantomData, tree: PhantomData }
+        AvlForest { arena, key: PhantomData, value: PhantomData, tree: PhantomData, into: PhantomData }
     }
 }
 
-impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> AvlForest<T, K, V, A> {
+impl<T: Copy, K: Copy + PartialOrd, V: Copy, N: Copy + From<AvlNode<T, K, V>>, A: NodeArena<N>> AvlForest<T, K, V, N, A>
+where
+    N: Into<AvlNode<T, K, V>>,
+    for<'a> &'a N: Into<&'a AvlNode<T, K, V>>,
+    for<'a> &'a mut N: Into<&'a mut AvlNode<T, K, V>>,
+{
     pub fn append(&mut self, value: T) -> Option<u32> {
         // return the index of the newly inserted node as the root of the tree
-        self.arena.insert(Node::tree(value))
+        self.arena.insert(N::from(Node::tree(value)))
     }
 
     pub fn insert(&mut self, tree: u32, key: K, value: V) -> Option<u32> {
         // allocate a new node in the arena
-        let idx = self.arena.insert(Node::node(key, value))?;
+        let idx = self.arena.insert(N::from(Node::node(key, value)))?;
 
         // find the root of the tree
-        let parent = unsafe { self.arena.get_unchecked(tree).0.get_root() };
+        let parent = unsafe { self.arena.get_unchecked(tree).into().0.get_root() };
 
         // trigger recursive insertion, may rotate the root
         let rotated = unsafe { self.insert_recursive(parent, idx, key) };
 
         // update the root of the tree if it was rotated
         if parent != rotated {
-            unsafe { self.arena.get_unchecked_mut(tree).0.set_root(rotated) };
+            unsafe { self.arena.get_unchecked_mut(tree).into().0.set_root(rotated) };
         }
 
         // return the index of the newly inserted node
@@ -140,14 +146,14 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> Avl
 
     pub fn height(&self, tree: u32) -> u32 {
         let mut height = 0;
-        let mut idx = unsafe { self.arena.get_unchecked(tree).0.get_root() };
+        let mut idx = unsafe { self.arena.get_unchecked(tree).into().0.get_root() };
 
         unsafe {
             while idx > 0 {
                 height += 1;
-                idx = match self.arena.get_unchecked(idx).0.get_balance() {
-                    Ordering::Equal | Ordering::Less => self.arena.get_unchecked(idx).0.get_left(),
-                    Ordering::Greater => self.arena.get_unchecked(idx).0.get_right(),
+                idx = match self.arena.get_unchecked(idx).into().0.get_balance() {
+                    Ordering::Equal | Ordering::Less => self.arena.get_unchecked(idx).into().0.get_left(),
+                    Ordering::Greater => self.arena.get_unchecked(idx).into().0.get_right(),
                 }
             }
         }
@@ -164,18 +170,18 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> Avl
         // we are ok with copying the parent node here, the variable is read-only
         let (idx, parent) = (parent, self.arena.get_unchecked(parent).clone());
 
-        if key <= parent.0.get_key() {
-            let left = self.insert_recursive(parent.0.get_left(), node, key);
-            self.arena.get_unchecked_mut(idx).0.set_left(left);
+        if key <= parent.into().0.get_key() {
+            let left = self.insert_recursive(parent.into().0.get_left(), node, key);
+            self.arena.get_unchecked_mut(idx).into().0.set_left(left);
 
-            match self.arena.get_unchecked(idx).0.get_balance() {
+            match self.arena.get_unchecked(idx).into().0.get_balance() {
                 Ordering::Equal => {
-                    self.arena.get_unchecked_mut(idx).0.set_balance(Ordering::Less);
+                    self.arena.get_unchecked_mut(idx).into().0.set_balance(Ordering::Less);
                 }
                 Ordering::Greater => {
-                    self.arena.get_unchecked_mut(idx).0.set_balance(Ordering::Equal);
+                    self.arena.get_unchecked_mut(idx).into().0.set_balance(Ordering::Equal);
                 }
-                Ordering::Less => match self.arena.get_unchecked(left).0.get_balance() {
+                Ordering::Less => match self.arena.get_unchecked(left).into().0.get_balance() {
                     Ordering::Less | Ordering::Equal => {
                         return self.rotate_ll(idx);
                     }
@@ -185,17 +191,17 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> Avl
                 },
             }
         } else {
-            let right = self.insert_recursive(parent.0.get_right(), node, key);
-            self.arena.get_unchecked_mut(idx).0.set_right(right);
+            let right = self.insert_recursive(parent.into().0.get_right(), node, key);
+            self.arena.get_unchecked_mut(idx).into().0.set_right(right);
 
-            match self.arena.get_unchecked(idx).0.get_balance() {
+            match self.arena.get_unchecked(idx).into().0.get_balance() {
                 Ordering::Equal => {
-                    self.arena.get_unchecked_mut(idx).0.set_balance(Ordering::Greater);
+                    self.arena.get_unchecked_mut(idx).into().0.set_balance(Ordering::Greater);
                 }
                 Ordering::Less => {
-                    self.arena.get_unchecked_mut(idx).0.set_balance(Ordering::Equal);
+                    self.arena.get_unchecked_mut(idx).into().0.set_balance(Ordering::Equal);
                 }
-                Ordering::Greater => match self.arena.get_unchecked(right).0.get_balance() {
+                Ordering::Greater => match self.arena.get_unchecked(right).into().0.get_balance() {
                     Ordering::Greater | Ordering::Equal => {
                         return self.rotate_rr(idx);
                     }
@@ -220,15 +226,15 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> Avl
     //   (0) x   a (?)              (?) a   b (?)
     //
     unsafe fn rotate_ll(&mut self, z: u32) -> u32 {
-        let y = self.arena.get_unchecked(z).0.get_left();
-        let a = self.arena.get_unchecked(y).0.get_right();
+        let y = self.arena.get_unchecked(z).into().0.get_left();
+        let a = self.arena.get_unchecked(y).into().0.get_right();
         let balance = if a != 0 { Ordering::Less } else { Ordering::Equal };
 
-        self.arena.get_unchecked_mut(z).0.set_left(a);
-        self.arena.get_unchecked_mut(z).0.set_balance(balance);
+        self.arena.get_unchecked_mut(z).into().0.set_left(a);
+        self.arena.get_unchecked_mut(z).into().0.set_balance(balance);
 
-        self.arena.get_unchecked_mut(y).0.set_right(z);
-        self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Equal);
+        self.arena.get_unchecked_mut(y).into().0.set_right(z);
+        self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Equal);
 
         // return y as the new root of the subtree
         return y;
@@ -248,37 +254,37 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> Avl
     //
     unsafe fn rotate_lr(&mut self, z: u32) -> u32 {
         // extract all nodes involved in the rotations
-        let y = self.arena.get_unchecked(z).0.get_left();
-        let x = self.arena.get_unchecked(y).0.get_right();
-        let a = self.arena.get_unchecked(x).0.get_left();
-        let b = self.arena.get_unchecked(x).0.get_right();
+        let y = self.arena.get_unchecked(z).into().0.get_left();
+        let x = self.arena.get_unchecked(y).into().0.get_right();
+        let a = self.arena.get_unchecked(x).into().0.get_left();
+        let b = self.arena.get_unchecked(x).into().0.get_right();
 
         // first rotation: y becomes left child of x
-        self.arena.get_unchecked_mut(y).0.set_right(a);
-        self.arena.get_unchecked_mut(x).0.set_left(y);
+        self.arena.get_unchecked_mut(y).into().0.set_right(a);
+        self.arena.get_unchecked_mut(x).into().0.set_left(y);
 
         // second rotation: x becomes new root of the subtree
-        self.arena.get_unchecked_mut(z).0.set_left(b);
-        self.arena.get_unchecked_mut(x).0.set_right(z);
+        self.arena.get_unchecked_mut(z).into().0.set_left(b);
+        self.arena.get_unchecked_mut(x).into().0.set_right(z);
 
         // adjust all balances
-        match self.arena.get_unchecked(x).0.get_balance() {
+        match self.arena.get_unchecked(x).into().0.get_balance() {
             Ordering::Less => {
-                self.arena.get_unchecked_mut(z).0.set_balance(Ordering::Greater);
-                self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(z).into().0.set_balance(Ordering::Greater);
+                self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Equal);
             }
             Ordering::Greater => {
-                self.arena.get_unchecked_mut(z).0.set_balance(Ordering::Equal);
-                self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Less);
+                self.arena.get_unchecked_mut(z).into().0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Less);
             }
             Ordering::Equal => {
-                self.arena.get_unchecked_mut(z).0.set_balance(Ordering::Equal);
-                self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(z).into().0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Equal);
             }
         }
 
         // not forget about the balance of x
-        self.arena.get_unchecked_mut(x).0.set_balance(Ordering::Equal);
+        self.arena.get_unchecked_mut(x).into().0.set_balance(Ordering::Equal);
 
         // and x becomes the new root of the subtree
         return x;
@@ -295,15 +301,15 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> Avl
     //      (?) a   x (0)            (?) b   a (?)
     //
     unsafe fn rotate_rr(&mut self, z: u32) -> u32 {
-        let y = self.arena.get_unchecked(z).0.get_right();
-        let a = self.arena.get_unchecked(y).0.get_left();
+        let y = self.arena.get_unchecked(z).into().0.get_right();
+        let a = self.arena.get_unchecked(y).into().0.get_left();
         let balance = if a != 0 { Ordering::Greater } else { Ordering::Equal };
 
-        self.arena.get_unchecked_mut(z).0.set_right(a);
-        self.arena.get_unchecked_mut(z).0.set_balance(balance);
+        self.arena.get_unchecked_mut(z).into().0.set_right(a);
+        self.arena.get_unchecked_mut(z).into().0.set_balance(balance);
 
-        self.arena.get_unchecked_mut(y).0.set_left(z);
-        self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Equal);
+        self.arena.get_unchecked_mut(y).into().0.set_left(z);
+        self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Equal);
 
         // return y as the new root of the subtree
         return y;
@@ -323,36 +329,36 @@ impl<T: Copy, K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<T, K, V>>> Avl
     //
     unsafe fn rotate_rl(&mut self, z: u32) -> u32 {
         // extract all nodes involved in the rotations
-        let y = self.arena.get_unchecked(z).0.get_right();
-        let x = self.arena.get_unchecked(y).0.get_left();
-        let a = self.arena.get_unchecked(x).0.get_left();
-        let b = self.arena.get_unchecked(x).0.get_right();
+        let y = self.arena.get_unchecked(z).into().0.get_right();
+        let x = self.arena.get_unchecked(y).into().0.get_left();
+        let a = self.arena.get_unchecked(x).into().0.get_left();
+        let b = self.arena.get_unchecked(x).into().0.get_right();
 
         // first rotation: y becomes right child of x
-        self.arena.get_unchecked_mut(y).0.set_left(b);
-        self.arena.get_unchecked_mut(x).0.set_right(y);
+        self.arena.get_unchecked_mut(y).into().0.set_left(b);
+        self.arena.get_unchecked_mut(x).into().0.set_right(y);
 
         // second rotation: x becomes new root of subtree
-        self.arena.get_unchecked_mut(z).0.set_right(a);
-        self.arena.get_unchecked_mut(x).0.set_left(z);
+        self.arena.get_unchecked_mut(z).into().0.set_right(a);
+        self.arena.get_unchecked_mut(x).into().0.set_left(z);
 
         // adjust balances
-        match self.arena.get_unchecked(x).0.get_balance() {
+        match self.arena.get_unchecked(x).into().0.get_balance() {
             Ordering::Less => {
-                self.arena.get_unchecked_mut(z).0.set_balance(Ordering::Equal);
-                self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Greater);
+                self.arena.get_unchecked_mut(z).into().0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Greater);
             }
             Ordering::Greater => {
-                self.arena.get_unchecked_mut(z).0.set_balance(Ordering::Less);
-                self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(z).into().0.set_balance(Ordering::Less);
+                self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Equal);
             }
             Ordering::Equal => {
-                self.arena.get_unchecked_mut(z).0.set_balance(Ordering::Equal);
-                self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(z).into().0.set_balance(Ordering::Equal);
+                self.arena.get_unchecked_mut(y).into().0.set_balance(Ordering::Equal);
             }
         }
 
-        self.arena.get_unchecked_mut(x).0.set_balance(Ordering::Equal);
+        self.arena.get_unchecked_mut(x).into().0.set_balance(Ordering::Equal);
         return x;
     }
 }
@@ -362,9 +368,54 @@ mod tests {
     use super::*;
     use crate::arena::NodeArray;
 
+    #[derive(Copy, Clone)]
+    union Foreign {
+        avl: AvlNode<i32, i32, i32>,
+        other: u32,
+    }
+
+    impl From<AvlNode<i32, i32, i32>> for Foreign {
+        fn from(node: AvlNode<i32, i32, i32>) -> Self {
+            Foreign { avl: node }
+        }
+    }
+
+    impl Into<AvlNode<i32, i32, i32>> for Foreign {
+        fn into(self) -> AvlNode<i32, i32, i32> {
+            unsafe { self.avl }
+        }
+    }
+
+    impl<'a> Into<&'a AvlNode<i32, i32, i32>> for &'a Foreign {
+        fn into(self) -> &'a AvlNode<i32, i32, i32> {
+            unsafe { &self.avl }
+        }
+    }
+
+    impl<'a> Into<&'a mut AvlNode<i32, i32, i32>> for &'a mut Foreign {
+        fn into(self) -> &'a mut AvlNode<i32, i32, i32> {
+            unsafe { &mut self.avl }
+        }
+    }
+
     #[test]
     fn can_create_avl_forest_with_a_new_root() {
         let arena: NodeArray<AvlNode<i32, i32, i32>, 10> = NodeArray::new();
+        let mut forest = AvlForest::new(arena);
+
+        let tree = forest.append(13);
+        assert!(tree.is_some());
+
+        let root = forest.insert(tree.unwrap(), 1, 2);
+        assert!(root.is_some());
+
+        let height = forest.height(tree.unwrap());
+        assert_eq!(height, 1);
+    }
+
+    #[test]
+    fn can_create_avl_forest_with_foreign_node() {
+        let arena: NodeArray<Foreign, 10> = NodeArray::new();
         let mut forest = AvlForest::new(arena);
 
         let tree = forest.append(13);
