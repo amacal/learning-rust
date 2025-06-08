1f19523642ca5885cc8d7b1d2ae4f807f0692f58
Adrian Macal <amacal@id5.io>
2025-06-08 08:43:24 +0000

    Introduce proper avl node.

--- src/avl.rs
+++ src/avl.rs
@@ -1,48 +1,120 @@
 use super::arena::NodeArena;
 use std::{cmp::Ordering, marker::PhantomData};
 
-pub trait AvlNode {
-    type KeyType: Copy + PartialOrd;
-    type ValueType: Copy;
-
-    fn new(key: Self::KeyType, value: Self::ValueType) -> Self;
-
-    fn get_key(&self) -> Self::KeyType;
-    fn get_value(&self) -> Self::ValueType;
-
-    fn get_balance(&self) -> Ordering;
-    fn set_balance(&mut self, balance: Ordering);
-
-    fn get_left(&self) -> u32;
-    fn set_left(&mut self, left: u32);
-
-    fn get_right(&self) -> u32;
-    fn set_right(&mut self, right: u32);
+#[derive(Copy, Clone)]
+struct Tree {
+    root: u32,
 }
 
-pub struct AvlForest<T: AvlNode + Copy, A: NodeArena<T>> {
-    arena: A,
-    item: PhantomData<T>,
+#[derive(Copy, Clone)]
+struct Item<K: Copy + PartialOrd, V: Copy> {
+    key: K,
+    value: V,
+    left: u32,
+    right: u32,
 }
 
-impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
-    pub fn new(arena: A) -> Self {
-        AvlForest { arena, item: PhantomData }
+#[derive(Copy, Clone)]
+union Node<K: Copy + PartialOrd, V: Copy> {
+    tree: Tree,
+    item: Item<K, V>,
+}
+
+#[derive(Copy, Clone)]
+pub struct AvlNode<K: Copy + PartialOrd, V: Copy>(Node<K, V>);
+
+impl<K: Copy + PartialOrd, V: Copy> Node<K, V> {
+    fn new(key: K, value: V) -> AvlNode<K, V> {
+        AvlNode(Node { item: Item { key, value, left: 0, right: 0 } })
+    }
+
+    fn get_root(&self) -> u32 {
+        unsafe { self.tree.root }
+    }
+
+    fn get_key(&self) -> K {
+        unsafe { self.item.key }
+    }
+
+    fn get_value(&self) -> V {
+        unsafe { self.item.value }
+    }
+
+    fn get_balance(&self) -> Ordering {
+        let left = unsafe { self.item.left & 0x80000000 };
+        let right = unsafe { self.item.right & 0x80000000 };
+
+        if left == right {
+            return Ordering::Equal;
+        }
+
+        if left > 0 {
+            return Ordering::Less;
+        }
+
+        return Ordering::Greater;
+    }
+
+    fn set_balance(&mut self, balance: Ordering) {
+        unsafe {
+            match balance {
+                Ordering::Equal => {
+                    self.item.left &= 0x7fffffff;
+                    self.item.right &= 0x7fffffff;
+                }
+                Ordering::Less => {
+                    self.item.left |= 0x80000000;
+                    self.item.right &= 0x7fffffff;
+                }
+                Ordering::Greater => {
+                    self.item.left &= 0x7fffffff;
+                    self.item.right |= 0x80000000;
+                }
+            }
+        }
+    }
+
+    fn get_left(&self) -> u32 {
+        unsafe { self.item.left & 0x7fffffff }
+    }
+
+    fn set_left(&mut self, left: u32) {
+        unsafe { self.item.left = left | self.item.left & 0x80000000 };
+    }
+
+    fn get_right(&self) -> u32 {
+        unsafe { self.item.right & 0x7fffffff }
+    }
+
+    fn set_right(&mut self, right: u32) {
+        unsafe { self.item.right = right | self.item.right & 0x80000000 };
     }
 }
 
-impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
-    pub fn append(&mut self, key: T::KeyType, value: T::ValueType) -> Option<u32> {
+pub struct AvlForest<K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<K, V>>> {
+    arena: A,
+    key: PhantomData<K>,
+    value: PhantomData<V>,
+}
+
+impl<K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<K, V>>> AvlForest<K, V, A> {
+    pub fn new(arena: A) -> Self {
+        AvlForest { arena, key: PhantomData, value: PhantomData }
+    }
+}
+
+impl<K: Copy + PartialOrd, V: Copy, A: NodeArena<AvlNode<K, V>>> AvlForest<K, V, A> {
+    pub fn append(&mut self, key: K, value: V) -> Option<u32> {
         // allocate a new node in the arena
-        let idx = self.arena.insert(T::new(key, value))?;
+        let idx = self.arena.insert(Node::new(key, value))?;
 
         // return the index of the newly inserted node as the root of the tree
         return Some(idx);
     }
 
-    pub fn insert(&mut self, tree: u32, key: T::KeyType, value: T::ValueType) -> Option<u32> {
+    pub fn insert(&mut self, tree: u32, key: K, value: V) -> Option<u32> {
         // allocate a new node in the arena
-        let idx = self.arena.insert(T::new(key, value))?;
+        let idx = self.arena.insert(Node::new(key, value))?;
 
         // trigger recursive insertion
         let root = unsafe { self.insert_recursive(tree, idx, key) };
@@ -58,9 +130,9 @@ impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
         unsafe {
             while idx > 0 {
                 height += 1;
-                idx = match self.arena.get_unchecked(idx).get_balance() {
-                    Ordering::Equal | Ordering::Less => self.arena.get_unchecked(idx).get_left(),
-                    Ordering::Greater => self.arena.get_unchecked(idx).get_right(),
+                idx = match self.arena.get_unchecked(idx).0.get_balance() {
+                    Ordering::Equal | Ordering::Less => self.arena.get_unchecked(idx).0.get_left(),
+                    Ordering::Greater => self.arena.get_unchecked(idx).0.get_right(),
                 }
             }
         }
@@ -68,7 +140,7 @@ impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
         return height;
     }
 
-    unsafe fn insert_recursive(&mut self, parent: u32, node: u32, key: T::KeyType) -> u32 {
+    unsafe fn insert_recursive(&mut self, parent: u32, node: u32, key: K) -> u32 {
         println!("Inserting node, parent {}, node {}", parent, node);
 
         // recursion base case
@@ -79,18 +151,18 @@ impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
         // we are ok with copying the parent node here, the variable is read-only
         let (idx, parent) = (parent, self.arena.get_unchecked(parent).clone());
 
-        if key <= parent.get_key() {
-            let left = self.insert_recursive(parent.get_left(), node, key);
-            self.arena.get_unchecked_mut(idx).set_left(left);
+        if key <= parent.0.get_key() {
+            let left = self.insert_recursive(parent.0.get_left(), node, key);
+            self.arena.get_unchecked_mut(idx).0.set_left(left);
 
-            match self.arena.get_unchecked(idx).get_balance() {
+            match self.arena.get_unchecked(idx).0.get_balance() {
                 Ordering::Equal => {
                     println!("Setting balance to Less for node {}", idx);
-                    self.arena.get_unchecked_mut(idx).set_balance(Ordering::Less);
+                    self.arena.get_unchecked_mut(idx).0.set_balance(Ordering::Less);
                 }
                 Ordering::Greater => {
                     println!("Setting balance to Equal for node {}", idx);
-                    self.arena.get_unchecked_mut(idx).set_balance(Ordering::Equal);
+                    self.arena.get_unchecked_mut(idx).0.set_balance(Ordering::Equal);
                 }
                 Ordering::Less => {
                     println!("Node {} is already left heavy, rotating", idx);
@@ -98,8 +170,8 @@ impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
                 }
             }
         } else {
-            let right = self.insert_recursive(parent.get_right(), node, key);
-            self.arena.get_unchecked_mut(idx).set_right(right);
+            let right = self.insert_recursive(parent.0.get_right(), node, key);
+            self.arena.get_unchecked_mut(idx).0.set_right(right);
         }
 
         return idx;
@@ -111,18 +183,15 @@ impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
     //      / \                  /
     // (0) x   a (?)            a (?)
     unsafe fn rotate_ll(&mut self, z: u32) -> u32 {
-        let y = self.arena.get_unchecked(z).get_left();
-        let a = self.arena.get_unchecked(y).get_right();
-        let x = self.arena.get_unchecked(y).get_left();
+        let y = self.arena.get_unchecked(z).0.get_left();
+        let a = self.arena.get_unchecked(y).0.get_right();
         let balance = if a != 0 { Ordering::Less } else { Ordering::Equal };
 
-        println!("Rotating LL at node {} {} {}", z, y, x);
+        self.arena.get_unchecked_mut(z).0.set_left(a);
+        self.arena.get_unchecked_mut(z).0.set_balance(balance);
 
-        self.arena.get_unchecked_mut(z).set_left(a);
-        self.arena.get_unchecked_mut(z).set_balance(balance);
-
-        self.arena.get_unchecked_mut(y).set_right(z);
-        self.arena.get_unchecked_mut(y).set_balance(Ordering::Equal);
+        self.arena.get_unchecked_mut(y).0.set_right(z);
+        self.arena.get_unchecked_mut(y).0.set_balance(Ordering::Equal);
 
         // return new root
         return y;
@@ -131,89 +200,12 @@ impl<T: AvlNode + Copy, A: NodeArena<T>> AvlForest<T, A> {
 
 #[cfg(test)]
 mod tests {
-    use std::cmp::Ordering;
-
     use super::*;
     use crate::arena::NodeArray;
 
-    #[derive(Copy, Clone)]
-    struct TestNode {
-        key: i32,
-        value: i32,
-        left: u32,
-        right: u32,
-    }
-
-    impl AvlNode for TestNode {
-        type KeyType = i32;
-        type ValueType = i32;
-
-        fn new(key: Self::KeyType, value: Self::ValueType) -> Self {
-            TestNode { key, value, left: 0, right: 0 }
-        }
-
-        fn get_key(&self) -> Self::KeyType {
-            self.key
-        }
-
-        fn get_value(&self) -> Self::ValueType {
-            self.value
-        }
-
-        fn get_balance(&self) -> Ordering {
-            let left = self.left & 0x80000000;
-            let right = self.right & 0x80000000;
-
-            if left == right {
-                return Ordering::Equal;
-            }
-
-            // left heavy
-            if left > 0 {
-                return Ordering::Less;
-            }
-
-            // right heavy
-            return Ordering::Greater;
-        }
-
-        fn set_balance(&mut self, balance: Ordering) {
-            match balance {
-                Ordering::Equal => {
-                    self.left &= 0x7fffffff;
-                    self.right &= 0x7fffffff;
-                }
-                Ordering::Less => {
-                    self.left |= 0x80000000;
-                    self.right &= 0x7fffffff;
-                }
-                Ordering::Greater => {
-                    self.left &= 0x7fffffff;
-                    self.right |= 0x80000000;
-                }
-            }
-        }
-
-        fn get_left(&self) -> u32 {
-            self.left & 0x7fffffff
-        }
-
-        fn set_left(&mut self, left: u32) {
-            self.left = left | self.left & 0x80000000;
-        }
-
-        fn get_right(&self) -> u32 {
-            self.right & 0x7fffffff
-        }
-
-        fn set_right(&mut self, right: u32) {
-            self.right = right | self.right & 0x80000000;
-        }
-    }
-
     #[test]
     fn can_create_avl_forest_with_a_new_root() {
-        let arena: NodeArray<TestNode, 10> = NodeArray::new();
+        let arena: NodeArray<AvlNode<i32, i32>, 10> = NodeArray::new();
         let mut forest = AvlForest::new(arena);
 
         let root = forest.append(1, 2);
@@ -225,7 +217,7 @@ mod tests {
 
     #[test]
     fn can_insert_nodes_into_avl_forest_ll() {
-        let arena: NodeArray<TestNode, 10> = NodeArray::new();
+        let arena: NodeArray<AvlNode<i32, i32>, 10> = NodeArray::new();
         let mut forest = AvlForest::new(arena);
 
         let root = forest.append(30, 300).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,11 @@
 mod arena;
 mod avl;
 
-use std::cmp::Ordering;
-
 use arena::NodeArray;
 use avl::{AvlForest, AvlNode};
 
-#[derive(Copy, Clone)]
-struct TestNode {
-    key: i32,
-    value: i32,
-    left: u32,
-    right: u32,
-}
-
-impl AvlNode for TestNode {
-    type KeyType = i32;
-    type ValueType = i32;
-
-    fn new(key: Self::KeyType, value: Self::ValueType) -> Self {
-        TestNode { key, value, left: 0, right: 0 }
-    }
-
-    fn get_key(&self) -> Self::KeyType {
-        self.key
-    }
-
-    fn get_value(&self) -> Self::ValueType {
-        self.value
-    }
-
-    fn get_balance(&self) -> Ordering {
-        let left = self.left & 0x80000000;
-        let right = self.right & 0x80000000;
-
-        if left == right {
-            return Ordering::Equal;
-        }
-
-        if left > 0 {
-            return Ordering::Less;
-        }
-
-        return Ordering::Greater;
-    }
-
-    fn set_balance(&mut self, balance: Ordering) {
-        match balance {
-            Ordering::Equal => {
-                self.left &= 0x7fffffff;
-                self.right &= 0x7fffffff;
-            }
-            Ordering::Less => {
-                self.left |= 0x80000000;
-                self.right &= 0x7fffffff;
-            }
-            Ordering::Greater => {
-                self.left &= 0x7fffffff;
-                self.right |= 0x80000000;
-            }
-        }
-    }
-
-    fn get_left(&self) -> u32 {
-        self.left & 0x7fffffff
-    }
-
-    fn set_left(&mut self, left: u32) {
-        self.left = left | self.left & 0x80000000;
-    }
-
-    fn get_right(&self) -> u32 {
-        self.right & 0x7fffffff
-    }
-
-    fn set_right(&mut self, right: u32) {
-        self.right = right | self.right & 0x80000000;
-    }
-}
-
 fn main() {
-    let arena: NodeArray<TestNode, 10> = NodeArray::new();
+    let arena: NodeArray<AvlNode<i32, i32>, 10> = NodeArray::new();
     let mut forest = AvlForest::new(arena);
 
     let root = forest.append(10, 100).unwrap();
